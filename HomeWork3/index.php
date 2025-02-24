bfb89e0367cc1fb8074675517c6f873eda87e7f4
Nikita Mishin <nikita.mishin.91@list.ru>
2025-02-24 08:56:14 +0300

    Homework 3, add readline() and setting up encoding for Russian text output

--- HomeWork3/index.php
+++ HomeWork3/index.php
@@ -1,20 +1,34 @@
 <?php
 
-$name = 'иван';
-$secondName = 'иванов';
-$lastName = 'иванович';
+ini_set('default_charset','UTF-8');
+mb_internal_encoding('UTF-8'); 
+
+$name = readline("Введите имя: ");
+$secondName = readline("Введите фамилию: ");
+$lastName = readline("Введите отчество: ");
+
+//echo "Отладка: Имя='$name', Фамилия='$secondName', Отчество='$lastName'\n";
+
+if (empty($name) || empty($secondName) || empty($lastName)) {
+    fwrite(STDERR, "Ошибка: Все поля должны быть заполнены!\n");
+    exit(1);
+}
 
 $inputData = $name . ' ' . $secondName . ' ' . $lastName;
+echo "Введенные данные: " . $inputData . "\n";
+
 $fullNameData = $secondName . ' ' . $name . ' ' . $lastName;
 $fullName = mb_convert_case($fullNameData, MB_CASE_TITLE, "UTF-8");
-$arName = explode(" ", mb_convert_case($inputData, MB_CASE_TITLE, "UTF-8"));
-$surnameAndInitials = $arName[1] . " " . mb_substr($arName[0], 0, 1) . "." .  mb_substr($arName[2], 0, 1) . ".";
+
+$arName = explode(" ", $fullName);
+
+$surnameAndInitials = $arName[0] . " " . mb_substr($arName[1], 0, 1) . "." . mb_substr($arName[2], 0, 1) . ".";
 $fio = "";
 
 foreach ($arName as $word) {
     $fio .= mb_substr($word, 0, 1);
 }
 
-echo 'Полное имя: ' . "'" . $fullName . "'" . "\r\n";
-echo 'Фамилия и инициалы: ' . "'" . $surnameAndInitials . "'" . "\r\n";
-echo 'Аббревиатура: ' . "'" . $fio . "'" . "\r\n";
+echo 'Полное имя: ' . "'" . $fullName . "'" . "\n";
+echo 'Фамилия и инициалы: ' . "'" . $surnameAndInitials . "'" . "\n";
+echo 'Аббревиатура: ' . "'" . $fio . "'" . "\n";
